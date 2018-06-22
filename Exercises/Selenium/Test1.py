23e92063f4c145511c2d419469fb982fb451f717
tomergilor <tomer.gr@gmail.com>
2018-06-22 21:29:04 +0300

    added files

--- Exercises/Selenium/Test1.py
+++ Exercises/Selenium/Test1.py
@@ -1,21 +1,47 @@
-import time
+# import time
+# from selenium import webdriver
+#
+# driver = webdriver.Firefox()
+#
+# driver.get('https://www.yahoo.com/')
+#
+#
+# driver.find_element_by_id('uh-search-box').send_keys('software testing')
+# driver.maximize_window()
+# #driver.implicitly_wait(20)
+# driver.find_element_by_id("uh-search-button").click()
+#
+# driver.find_element_by_id("yschsp").clear()
+#
+#
+# # Wait for 5 seconds
+# time.sleep(5)
+#
+#
+# driver.quit()
+
+print ("hi selenium")
+#open Firefox Browser an navigate to google home page
 from selenium import webdriver
+# invoking firefox Browser
+driver= webdriver.Firefox()
+#hit the URL
+driver.get("http://google.com")
 
-driver = webdriver.Firefox()
-
-driver.get('https://www.yahoo.com/')
-
-
-driver.find_element_by_id('uh-search-box').send_keys('software testing')
+print(driver.current_url)
+print(driver.title)
+print("Navigaating to gmail")
+driver.get("http://gmail.com")
+print(driver.title)
+driver.back()
+print("Came back to google home page")
+print(driver.title)
+driver.forward()
+print("Came back to gmail home page")
+print(driver.title)
+driver.refresh()
 driver.maximize_window()
-#driver.implicitly_wait(20)
-driver.find_element_by_id("uh-search-button").click()
-
-driver.find_element_by_id("yschsp").clear()
-
-
-# Wait for 5 seconds
-time.sleep(5)
-
-
+# driver.close()
 driver.quit()
+
+
